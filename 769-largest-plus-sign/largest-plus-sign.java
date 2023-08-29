433af8ea1d613030c09523a0e3f314b268f882de
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-29 14:41:12 +0530

    Time: 25 ms (86.50%) | Memory: 45.4 MB (94.00%) - LeetSync

--- 769-largest-plus-sign/largest-plus-sign.java
+++ 769-largest-plus-sign/largest-plus-sign.java
@@ -1,45 +1,26 @@
 class Solution {
-    public int orderOfLargestPlusSign(int N, int[][] mines) {
-    int[][] grid = new int[N][N];   
-    for (int i = 0; i < N; i++) {
-        Arrays.fill(grid[i], N);
-    }    
-    for (int[] m : mines) {
-        grid[m[0]][m[1]] = 0;
-    }    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0, k = N - 1, l = 0, r = 0, u = 0, d = 0; j < N; j++, k--) {
-            grid[i][j] = Math.min(grid[i][j], l = (grid[i][j] == 0 ? 0 : l + 1));  // left direction
-    /**              j is a column index, iterate from left to right
-                     every time check how far left it can reach.
-                     if grid[i][j] is 0, l needs to start over from 0 again, otherwise increment */
-            grid[i][k] = Math.min(grid[i][k], r = (grid[i][k] == 0 ? 0 : r + 1));  // right direction
-            /**   k is a column index, iterate from right to left
-                    // every time check how far right it can reach.
-                    // if grid[i][k] is 0, r needs to start over from 0 again, otherwise increment */
-            grid[j][i] = Math.min(grid[j][i], u = (grid[j][i] == 0 ? 0 : u + 1));  // up direction
-            /** j is a row index, iterate from top to bottom
-                    // every time check how far up it can reach.
-                    // if grid[j][i] is 0, u needs to start over from 0 again, otherwise increment */
-            grid[k][i] = Math.min(grid[k][i], d = (grid[k][i] == 0 ? 0 : d + 1));  // down direction
-            /* // k is a row index, iterate from bottom to top
-                    // every time check how far down it can reach.
-                    // if grid[k][i] is 0, d needs to start over from 0 again, otherwise increment*/
+    public int orderOfLargestPlusSign(int n, int[][] mines) {
+        int [][] grid = new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(grid[i],n);
         }
-    }    
-    int res = 0;    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            res = Math.max(res, grid[i][j]);
+        for(int m[] : mines){
+            grid[m[0]][m[1]]=0;
         }
-    }    
-    return res;
+        for(int i=0;i<n;i++){
+            for(int j=0,k=n-1,l=0,r=0,u=0,d=0;j<n;j++,k--){
+                grid[i][j]=Math.min(grid[i][j],l=grid[i][j]==0?0:l+1);
+                grid[i][k]=Math.min(grid[i][k],r=grid[i][k]==0?0:r+1);
+                grid[j][i]=Math.min(grid[j][i],u=grid[j][i]==0?0:u+1);
+                grid[k][i]=Math.min(grid[k][i],d=grid[k][i]==0?0:d+1);
+            }
+        }
+        int result=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                result=Math.max(result,grid[i][j]);
+            }
+        }
+        return result;
+    }
 }
-}
-/*For the left direction, i is a row index while j is a column index. For each row, j goes from left to right (increasing).
-
-For the right direction, i is a row index while k is a column index. For each row, k goes from right to left (decreasing).
-
-For the up direction, i is a column index while j is a row index. For each column, j goes from top to bottom (increasing).
-
-For the down direction, i is a column index while k is a row index. For each column, k goes from bottom to top (decreasing).*/
