ee47805f3fc792172c2ee52bdabdf884be2be686
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-29 15:24:55 +0530

    Time: 168 ms (75.53%) | Memory: 43.9 MB (70.46%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int lenLongestFibSubseq(int[] A) {
-        int N = A.length;
-        Set<Integer> S = new HashSet();
-        for (int x: A) S.add(x);
-
-        int ans = 0;
-        for (int i = 0; i < N; ++i)
-            for (int j = i+1; j < N; ++j) {
-                /* With the starting pair (A[i], A[j]),
-                 * y represents the future expected value in
-                 * the fibonacci subsequence, and x represents
-                 * the most current value found. */
-                int x = A[j], y = A[i] + A[j];
-                int length = 2;
-                while (S.contains(y)) {
-                    // x, y -> y, x+y
-                    int tmp = y;
-                    y += x;
-                    x = tmp;
-                    ans = Math.max(ans, ++length);
+    public int lenLongestFibSubseq(int[] arr) {
+        HashSet<Integer>set = new HashSet<>();
+        for(int i : arr){
+            set.add(i);
+        }
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                int x = arr[j],y=arr[i]+arr[j];
+                int length=2;
+                while(set.contains(y)){
+                    int temp = y;
+                    y+=x;
+                    x=temp;
+                    ans=Math.max(ans,++length);
                 }
             }
-
-        return ans >= 3 ? ans : 0;
+        }
+        return ans>=3?ans:0;
     }
 }
