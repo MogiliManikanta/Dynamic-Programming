8f868ff8a0a0ad1dcd4268438b59ee2df800534f
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-29 15:16:59 +0530

    Time: 177 ms (65.40%) | Memory: 43.9 MB (74.68%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,21 +1,24 @@
 class Solution {
     public int lenLongestFibSubseq(int[] A) {
         int N = A.length;
-        Map<Integer, Integer> index = new HashMap();
-        for (int i = 0; i < N; ++i)
-            index.put(A[i], i);
+        Set<Integer> S = new HashSet();
+        for (int x: A) S.add(x);
 
-        Map<Integer, Integer> longest = new HashMap();
         int ans = 0;
-
-        for (int k = 0; k < N; ++k)
-            for (int j = 0; j < k; ++j) {
-                int i = index.getOrDefault(A[k] - A[j], -1);
-                if (i >= 0 && i < j) {
-                    // Encoding tuple (i, j) as integer (i * N + j)
-                    int cand = longest.getOrDefault(i * N + j, 2) + 1;
-                    longest.put(j * N + k, cand);
-                    ans = Math.max(ans, cand);
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j) {
+                /* With the starting pair (A[i], A[j]),
+                 * y represents the future expected value in
+                 * the fibonacci subsequence, and x represents
+                 * the most current value found. */
+                int x = A[j], y = A[i] + A[j];
+                int length = 2;
+                while (S.contains(y)) {
+                    // x, y -> y, x+y
+                    int tmp = y;
+                    y += x;
+                    x = tmp;
+                    ans = Math.max(ans, ++length);
                 }
             }
 
