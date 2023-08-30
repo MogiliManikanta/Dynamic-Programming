c30d9a6692708208f9c5755d1dbb497cf8bec13e
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-30 09:28:31 +0530

    Time: 7 ms (68.48%) | Memory: 53.7 MB (87.01%) - LeetSync

---
+++ 221-maximal-square/maximal-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        if(rows==0){
+            return 0;
+        }
+        int largest=0;
+        int[][] dp = new int[rows+1][cols+1];
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=cols;j++){
+                if(matrix[i-1][j-1]=='1'){
+                    dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
+                    if(largest<dp[i][j]){
+                        largest=dp[i][j];
+                    }
+                }
+            }
+        }
+        return largest*largest;
+    }
+}
