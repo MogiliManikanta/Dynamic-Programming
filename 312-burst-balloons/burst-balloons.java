64d841800e531f00395ea77ca14a046f49e36cb5
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-30 19:27:44 +0530

    Time: 32 ms (95.22%) | Memory: 41 MB (98.91%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -21,11 +21,21 @@ class Solution {
        for(int i=0;i<n;i++){
            temp[i+1]=nums[i];
        }
-       int[][] dp = new int[n+1][n+1];
-       for(int i[] : dp){
-           Arrays.fill(i,-1);
+       int[][] dp = new int[n+2][n+2];
+       for(int i=n;i>=1;i--){
+           for(int j=1;j<=n;j++){
+               if(i>j){
+                   continue;
+               }
+            int maxi = Integer.MIN_VALUE;
+            for(int ind =i;ind<=j;ind++){
+                int cost = temp[i-1]*temp[ind]*temp[j+1]+dp[i][ind-1]+dp[ind+1][j];
+            maxi = Math.max(maxi,cost);
+        }
+        dp[i][j] = maxi;
+           }
        }
-       return f(1,n,temp,dp); 
+       return dp[1][n];
     }
 }
 /**The code given by the user is in C++ language and it is a solution for the problem of finding the maximum coins that can be obtained by bursting balloons. The code uses a recursive function f that takes the indices of the left and right boundaries of the balloons and a reference to the vector of balloon values. The function returns the maximum coins that can be obtained by bursting all the balloons between the boundaries. The function iterates over all the possible choices of the last balloon to burst and calculates the cost of that choice by multiplying the values of the left, right and last balloons and adding the recursive calls for the left and right subproblems. The function returns the maximum cost among all the choices. The main function maxCoins takes a vector of balloon values and adds 1 to both ends of the vector to represent the walls. It then calls the recursive function with 1 and n as the boundaries, where n is the original size of the vector. */
